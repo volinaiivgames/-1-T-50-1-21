b5d41ac022fc6243cdc4de6fdc7629cd287ab90d
NANO <72669313+volinaiivgames@users.noreply.github.com>
2022-10-12 03:09:43 +0300

    Практическая 1 - Калькулятор

---
+++ practic4/Pages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practic4
+{
+    class Pages
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public Pages(string name, string description, DateTime date)
+        {
+            Name = name;
+            Description = description;
+            Date = date;
+        }
+    }
+}
---
+++ practic4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practic4
+{
+    class Program
+    {
+        static int StartPage = 2;
+        static int PositionPage = 2;
+        static bool ActivePage = false;
+        static List<Pages> ListPages = new List<Pages>();
+        static List<Pages> ListPagesGarage = new List<Pages>();
+        static void Main(string[] args) {
+            ListPages.Add(new Pages("Придти на пары", "Со первой по пятую", new DateTime()));
+            ListPages.Add(new Pages("Сходить в гараж", "", new DateTime()));
+            ListPagesGarage.Add(new Pages("Проверить практические", "", new DateTime()));
+            //ListPages.Add(new Pages("Сходить в гараж", "", new DateTime()));
+            Open();
+        }
+
+        static void Open()
+        {
+            Console.Clear();
+            ActivePage = false;
+            DateTime date = new DateTime();
+            Console.WriteLine($"Выбрана дата {date}");
+            Console.WriteLine("------------------------------");
+            for (int i = 0; i < ListPages.Count; i++)
+            {
+                Console.WriteLine($"  {i+1}. {ListPages[i].Name}");
+            }
+
+            setCursor();
+            while (!Console.KeyAvailable) setCursor();
+        }
+
+        static void setCursor()
+        {
+            if (!ActivePage)
+            {
+                Console.SetCursorPosition(0, PositionPage);
+                Console.WriteLine("->");
+            }
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.UpArrow && PositionPage >= StartPage && !ActivePage)
+            {
+                PositionPage--;
+                Console.SetCursorPosition(0, PositionPage);
+                Console.WriteLine("->");
+                Open();
+            }
+            else if (key.Key == ConsoleKey.DownArrow && !ActivePage)
+            {
+                PositionPage++;
+                Console.SetCursorPosition(0, PositionPage);
+                Console.WriteLine("->");
+                Open();
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                if (!ActivePage) Enter();
+                else Open();
+                return;
+            }
+        }
+        static void Enter()
+        {
+            Console.Clear();
+            int next = PositionPage - StartPage;
+            if (next == 0)
+            {
+                Pages page = ListPages[next];
+                Console.WriteLine($"{page.Name}");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine($"Описание: {page.Description}");
+                Console.WriteLine($"Дата: {page.Date}");
+                ActivePage = true;
+            }
+            else if(next == 1)
+            {
+                Pages page = ListPages[1];
+                ActivePage = true;
+            }
+        }
+    }
+}
